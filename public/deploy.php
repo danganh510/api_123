b61a36382e87a0f87723202f4114d8808fc4fb5b
anh.dn <anh.dn@gianty.com.vn>
2023-03-23 16:37:02 +0700

    update small

--- public/deploy.php
+++ public/deploy.php
@@ -1,7 +1,7 @@
 <?php
 $content = file_get_contents("php://input");
 $json    = json_decode($content, true);
-file_put_contents("/test.txt",$json);
+file_put_contents("test.txt",$json);
 chdir('..');
 $output = shell_exec('git pull'); 
 echo "<pre>$output</pre>";
