58178d523fc0f61e59cd6cac96bc88a7615876ce
anh.dn <anh.dn@gianty.com.vn>
2023-03-27 14:16:25 +0700

    test

--- apps/backend/controllers/CrawlertourController.php
+++ apps/backend/controllers/CrawlertourController.php
@@ -73,7 +73,7 @@ class CrawlertourController extends ControllerBase
                 'is_live' => $is_live,
                 'tour' => true
             ];
-            die(json_encode($request));
+   
             $clientGuzzle = new \GuzzleHttp\Client();
             $url = 'http://123tiso.com/save-match';
             try {
