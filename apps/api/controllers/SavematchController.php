b0f0d18ba82be22b9d9afd42c22f314390cc863b
danganh <danganh.hna@gmail.com>
2023-04-01 21:08:41 +0700

    fix small

--- apps/api/controllers/SavematchController.php
+++ apps/api/controllers/SavematchController.php
@@ -90,7 +90,7 @@ class SavematchController extends ControllerBase
         // }
         delete_cache:
 
-     var_dump($is_live);exit;
+  
         if (($is_live !== true)) {
             $timestamp_before_7 = time() - 7 * 24 * 60 * 60 + 60 * 60; //backup 1h
             $timestamp_affter_7 = time() + 7 * 24 * 60 * 60 + 60 * 60; //backup 1h
