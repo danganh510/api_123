fba82fa586b7c8075ddb86127a9a70f30e18ec32
danganh <danganh.hna@gmail.com>
2023-04-02 22:02:46 +0700

    fix small

--- apps/repositories/MatchRepo.php
+++ apps/repositories/MatchRepo.php
@@ -143,6 +143,7 @@ class MatchRepo extends Component
                 'is_new' => $is_new
             ];
         } else {
+            var_dump($matchSave->getMessages());exit;
             return [
                 'messages' => $matchSave->getMessages()
             ];
