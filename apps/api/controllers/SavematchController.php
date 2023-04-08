6b6db8c373b5b0023a6bb8a0dcbaac18960ef2fb
danganh <danganh.hna@gmail.com>
2023-04-08 18:57:49 +0700

    fix small

--- apps/api/controllers/SavematchController.php
+++ apps/api/controllers/SavematchController.php
@@ -73,6 +73,9 @@ class SavematchController extends ControllerBase
             }
             $result =  $matchRepo->saveMatch($match, $home, $away, $tournament, $time_plus, $this->type_crawl);
             if (isset($result['matchSave'])) {
+                if ($result['matchSave'] == false) {
+                    var_dump($result['matchSave']->getMessages());exit;
+                }
                 echo json_encode($result['matchSave']);
                 if (isset($result['matchSave']->match_id)) {
                     $arrMatchIdLive[] = $result['matchSave']->match_id;
