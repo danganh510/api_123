44916d44743f45d8a38742baa819965587f1cc6f
danganh <danganh.hna@gmail.com>
2023-04-08 21:13:00 +0700

    fix msall

--- apps/api/controllers/SavematchController.php
+++ apps/api/controllers/SavematchController.php
@@ -103,15 +103,27 @@ class SavematchController extends ControllerBase
             $matchCache = new CacheMatch();
             $matchCache->setCache(json_encode($arrMatch));
         } else {
-            $cache = new CacheMatchIdLive();
-            $arrMatchIdLive = $cache->getCache();
+            // $cache = new CacheMatchIdLive();
+            // $arrMatchIdLive = $cache->getCache();
 
-            $arrMatch = ScMatch::find([
-                'FIND_IN_SET(match_id,:arrId:)',
-                'bind' => [
-                    'arrId' => implode(",", $arrMatchIdLive)
-                ]
-            ]);
+            // $arrMatch = ScMatch::find([
+            //     'FIND_IN_SET(match_id,:arrId:)',
+            //     'bind' => [
+            //         'arrId' => implode(",", $arrMatchIdLive)
+            //     ]
+            // ]);
+            // $arrMatch = $arrMatch->toArray();
+            // $matchCache = new CacheMatchLive();
+            // $result = $matchCache->setCache(json_encode($arrMatch));
+
+            $time_end = time() + 3 * 60;
+            $time_begin = time() - 3 * 60;
+            $time_now = time();
+            $arrMatch = ScMatch::find(
+                "match_status = 'S' OR 
+                    (match_status = 'F' AND match_time_finish < $time_end  AND match_time_finish > $time_now) 
+                    OR (match_status = 'W' AND match_start_time > $time_begin AND match_start_time < $time_now) "
+            );
             $arrMatch = $arrMatch->toArray();
             $matchCache = new CacheMatchLive();
             $result = $matchCache->setCache(json_encode($arrMatch));
