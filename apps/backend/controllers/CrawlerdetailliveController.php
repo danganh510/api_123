5d33bd8410029b403434e1621cd460f4e689ade5
anh.dn <anh.dn@gianty.com.vn>
2023-05-04 09:22:23 +0700

    fix small

--- apps/backend/controllers/CrawlerdetailliveController.php
+++ apps/backend/controllers/CrawlerdetailliveController.php
@@ -90,7 +90,7 @@ class CrawlerdetailliveController extends ControllerBase
         if ($result) {
             echo "crawl succes--";
         }
-   var_dump($detail['match']['timeNow'] == "Postponed");exit;
+   var_dump($detail['match']['timeNow']);exit;
    
         //lưu thông tin mới của match
         if (
