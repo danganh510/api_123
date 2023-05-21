169adc52f6894ee2a85bb48f07d888e317d0bb56
danganh <danganh.hna@gmail.com>
2023-05-21 22:44:46 +0700

    update

--- apps/repositories/MatchRepo.php
+++ apps/repositories/MatchRepo.php
@@ -410,10 +410,10 @@ class MatchRepo extends Component
     public static function getMatchToday()
     {
         $today = strtotime(strftime('%Y-%m-%d', time()));
-        $start_day = $today - 7 * 60 * 60;
+        $start_day = $today + 7 * 60 * 60;
         //thời gian bonus là +- 160 phút
         $bonus_start_day = $start_day - 160 * 60;
-        $end_day = $today + 24 * 60 * 60;
+        $end_day = $start_day + 24 * 60 * 60 ;
         //thời gian bonus là +- 160 phút
         $bonus_end_day = $end_day + 160 * 60;
         $arrMatch = ScMatch::find([
