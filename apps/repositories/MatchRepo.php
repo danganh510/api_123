87e0f58b062106cd4e70d78dcc5238dc0321c7e0
danganh <danganh.hna@gmail.com>
2023-05-21 22:47:46 +0700

    fix time zone

--- apps/repositories/MatchRepo.php
+++ apps/repositories/MatchRepo.php
@@ -409,8 +409,8 @@ class MatchRepo extends Component
     }
     public static function getMatchToday()
     {
-        $today = strtotime(strftime('%Y-%m-%d', time()));
-        $start_day = $today + 7 * 60 * 60;
+        $today = strtotime(strftime('%Y-%m-%d', time() + 7 * 60 * 60));
+        $start_day = $today;
         //thời gian bonus là +- 160 phút
         $bonus_start_day = $start_day - 160 * 60;
         $end_day = $start_day + 24 * 60 * 60 ;
