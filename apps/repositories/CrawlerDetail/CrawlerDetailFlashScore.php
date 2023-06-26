046b7d76401dc6574c61870a2b62897bc0f3d224
danganh <danganh.hna@gmail.com>
2023-06-26 18:14:31 +0700

    test

--- apps/repositories/CrawlerDetail/CrawlerDetailFlashScore.php
+++ apps/repositories/CrawlerDetail/CrawlerDetailFlashScore.php
@@ -235,6 +235,7 @@ class CrawlerDetailFlashScore extends CrawlerDetail
     {
         $info = [];
         $divCrawl = str_get_html($this->divInfo);
+        var_dump($divCrawl);exit;
         if (!$divCrawl) {
             return [];
         }
